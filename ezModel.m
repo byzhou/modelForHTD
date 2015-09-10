2b54327ffd8c606d0970136043852a38bfe9443b
bob-icsg <bobzhou@bu.edu>
2015-09-09 21:31:46 -0400

    modified simple model

--- ezModel.m
+++ ezModel.m
@@ -17,6 +17,7 @@ x_withNoise     = x_noiseFree + x_noise ;
 covariance      = cov (x_noiseFree , x_withNoise);
 rho             = covariance(1 , 2) / sqrt (covariance(1 , 1) * covariance(2 , 2)) ;
 
+% I use the end+1 to add more rho values into the rho queue
 for i = 1 : (testNum - 1)
     x_noise         = normrnd (0, noiseStrength, dimension, dimension);
     x_withNoise     = x_noiseFree + x_noise ;
@@ -25,4 +26,4 @@ for i = 1 : (testNum - 1)
 end
 
 % show the histogram of the distribution
-hist(rho);
+hist(rho , [0.9:0.01:1]);
